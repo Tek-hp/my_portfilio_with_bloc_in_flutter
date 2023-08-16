73dfca9405af744d0ec505cc3a880367b86affea
Calm_0.0_ <tekrajojha6@gmail.com>
2023-08-16 12:24:47 +0545

    added launcher icon

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_web_plugins/flutter_web_plugins.dart';
 
 import 'bloc_observer.dart';
 import 'portfolio_app.dart';
 
 void main() {
+  usePathUrlStrategy();
   Bloc.observer = MyBlocObserver();
   runApp(const PortfolioApp());
 }
